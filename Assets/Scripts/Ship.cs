398cf5b98e40cccf34f2b83bf0792d4442c8e72b
AmirBasy <amir.basyouni@gmail.com>
2019-12-17 12:30:27 +0100

    First commit

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public Rigidbody rigibody;
+    public float rotationVelocity=45;
+    public float acceleration;
+    public GameObject shotReference;
+
+    void Rotate(float direction)
+    {
+        rigibody.AddTorque(transform.up * rotationVelocity * Time.deltaTime * direction);
+        //transform.Rotate(Vector3.up * rotationVelocity * Time.deltaTime * direction);
+    }
+
+    void Shot()
+    {
+        GameObject newShot = Instantiate(shotReference);
+        newShot.transform.position = transform.position + transform.forward*2;
+    }
+
+    void Accelerate()
+    {
+        rigibody.AddForce(transform.forward * acceleration * Time.deltaTime);
+        //transform.Translate(Vector3.forward * acceleration * Time.deltaTime);
+    }
+
+    void Die()
+    {
+
+    }
+
+    void CrossScreen()
+    {
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) Rotate(-1);
+        if (Input.GetKey(KeyCode.D)) Rotate(1);
+        if (Input.GetKey(KeyCode.W)) Accelerate();
+        if (Input.GetKeyDown(KeyCode.Space)) Shot();
+    }
+}
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    public float velocity = 50;
+    public Vector3 direction = Vector3.forward;
+
+
+    void Move()
+    {
+        transform.Translate(direction * velocity * Time.deltaTime, Space.World);
+    }
+
+    void SplitAsteroid()
+    {
+
+    }
+
+    void DestroyAsteroid()
+    {
+
+    }
+
+    void Die()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
